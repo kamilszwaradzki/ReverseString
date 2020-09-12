09289a48f21d10fc297486b5b1791396db685b90
Kamil Szwaradzki <kamil.szwaradzki@gmail.com>
2020-09-12 21:39:56 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        public static string Reverse( string s )
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse( charArray );
+            return new string( charArray );
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Reverse("Hello World!"));
+        }
+    }
+}
